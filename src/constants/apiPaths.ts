435eb65f177b1ae20dab0398068493fe01d81235
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2020-11-09 05:19:19 +0300

    Task-4: Update product API url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://n8r1uqzawh.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://n3rqi9815f.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'

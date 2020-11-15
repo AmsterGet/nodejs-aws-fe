74d3d8f202f676d735379255f998bcae2120e7ee
Ilya Hancharyk <Ilya_Hancharyk@epam.com>
2020-11-16 00:08:57 +0300

    Task-5: Update import URL

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 
 const API_PATHS = {
-  product: 'https://n3rqi9815f.execute-api.eu-west-1.amazonaws.com/dev/products',
+  product: 'https://n3rqi9815f.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://6tzakrlic9.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
